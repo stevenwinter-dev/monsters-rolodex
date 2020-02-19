e6f49fd0e311647f15eb35ff3af3c6f715c9b4a9
Steven Winter <stevenwinter.me@gmail.com>
2020-02-18 21:21:58 -0600

    Updates

---
+++ precache-manifest.8f759aafbf76860c1ccaef570cc6c4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b70a124bb69c88635144a528830525",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9d8b172a1b2b0a12ae66",
+    "url": "/github.io/monsters-rolodex/static/css/main.be87aff0.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/github.io/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/github.io/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8b172a1b2b0a12ae66",
+    "url": "/github.io/monsters-rolodex/static/js/main.fb2fe10b.chunk.js"
+  },
+  {
+    "revision": "b6030ce2f0898b5ec3f8",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.5868f27a.js"
+  }
+]);
